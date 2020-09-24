d201a537f6348a09abf3cefcbf75f2dbb1475664
Eric Joseph Bezerra Costa <frodoeric@hotmail.com>
2020-09-24 20:46:05 -0300

    criação contacorrente handler

--- src/devboost.challengeday.API/Controllers/ContaCorrenteController.cs
+++ src/devboost.challengeday.API/Controllers/ContaCorrenteController.cs
@@ -14,18 +14,21 @@ namespace devboost.challengeday.API.Controllers
         [HttpGet]
         public async Task<ActionResult> Get()
         {
+            //todo: trazer do mongo
             return Ok("saldo");
         }
 
         [HttpPost("depositar")]
         public async Task<ActionResult> Depositar([FromBody] ContaCorrenteRequest contaCorrenteRequest)
         {
+            //todo: mandar para o kafka
             return Ok("deposito efetuado");
         }
 
         [HttpPost("sacar")]
         public async Task<ActionResult> Sacar([FromBody] ContaCorrenteRequest contaCorrenteRequest)
         {
+            //todo: mandar para o kafka
             return Ok("deposito efetuado");
         }
     }
---
+++ src/devboost.challengeday.Domain/Interfaces/IContaCorrenteHandler.cs
@@ -0,0 +1,16 @@
+﻿using devboost.challengeday.Domain.Commands.Request;
+using devboost.challengeday.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace devboost.challengeday.Domain.Interfaces
+{
+    public interface IContaCorrenteHandler
+    {
+        Task DepositarValor(ContaCorrenteRequest contaCorrente);
+
+        Task SacarValor(ContaCorrenteRequest contaCorrente);
+    }
+}
