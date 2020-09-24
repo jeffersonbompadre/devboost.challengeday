157df424ca9f2ab70b6808cba803f49c15f1ac2e
Eric Joseph Bezerra Costa <frodoeric@hotmail.com>
2020-09-24 20:17:17 -0300

    inclusao controller conta corrente

---
+++ src/devboost.challengeday.API/Controllers/ContaCorrenteController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace devboost.challengeday.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContaCorrenteController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok("saldo");
+        }
+
+        [HttpPost("depositar")]
+        public async Task<ActionResult> depositar()
+        {
+            return Ok("deposito efetuado");
+        }
+
+        [HttpPost("sacar")]
+        public async Task<ActionResult> sacar()
+        {
+            return Ok("deposito efetuado");
+        }
+    }
+}
