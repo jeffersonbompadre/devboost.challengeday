c49adba47fcd084f1e5622de228a3698420c16c5
Robert Luiz <robert.tiweb@gmail.com>
2020-09-24 21:04:21 -0300

    wip: repositorio

--- src/devboost.challengeday.Infra/Repositorios/ContaCorrenteRepositorio.cs
+++ src/devboost.challengeday.Infra/Repositorios/ContaCorrenteRepositorio.cs
@@ -1,5 +1,7 @@
 ﻿namespace devboost.challengeday.Infra.Repositorios
 {
+    using System;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
 
     using devboost.challengeday.Domain.Interfaces;
@@ -12,19 +14,26 @@
     {
 
         private readonly IMongoCollection<ContaCorrenteDataModel> _ContaCorrenteCollection;
-        public async Task Deposito(ContaCorrente contaCorrente)
+
+
+        public ContaCorrenteRepositorio(IMongoDatabase database)
         {
-            throw new System.NotImplementedException();
+            _ContaCorrenteCollection = database.GetCollection<ContaCorrenteDataModel>("Pedido"); ;
         }
 
-        public async Task Saque(ContaCorrente contaCorrente)
+        public async Task<List<ContaCorrente>> GetAll(ContaCorrente contaCorrente)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
-        public async Task<decimal> Saldo(ContaCorrente contaCorrente)
+        public async Task<ContaCorrente> GetById(Guid id)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
+        }
+
+        public async Task<ContaCorrente> Save(ContaCorrente contaCorrente)
+        {
+            throw new NotImplementedException();
         }
     }
 }
