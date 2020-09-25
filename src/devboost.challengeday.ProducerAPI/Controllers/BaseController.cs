62c5fe4d28e5abcc942ae8c0dca663feb49c4c59
Ramsés França <ramsesfranca@hotmail.com>
2020-09-24 21:25:22 -0300

    Allteração

---
+++ src/devboost.challengeday.ProducerAPI/Controllers/BaseController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace devboost.challengeday.ProducerAPI.Controllers
+{
+    [ApiController, Route("api/v{version:apiVersion}/[controller]")]
+    public class BaseController : ControllerBase
+    {
+    }
+}
